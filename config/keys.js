e280d40b1f691bbf24ce5dca0486f7137be411ee
alijawad1511 <jawad.bukhari1511@gmail.com>
2022-03-04 21:13:54 +0500

    - cors module added - Display All items on Home Page from DB completed

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://jawad:jawad1511@cluster0.hbkld.mongodb.net/Shopping-Cart-MERN?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
+// DB Config
+const db = require('./config/keys').mongoURI;
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/Shopping-Cart')
+mongoose.connect(db)
     .then(() => console.log('Connected to MongoDB Successfully...'))
-    .catch((err) => console.log("Error : ", err));
+    .catch((err) => console.log("Error : ",err));
 
 // Route Files
 const items = require('./routes/api/items');
-app.use('/api/items', items);
+app.use('/api/items',items);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Shopping App Backend running at http://localhost:${PORT}`); });
+app.listen(PORT,() => { console.log(`Shopping App Backend running at http://localhost:${PORT}`); });
